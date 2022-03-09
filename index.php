6a383452124a5af2f595ae7db261df4a6de52737
sanjurajrh <84304865+sanjurajrh@users.noreply.github.com>
2022-03-09 12:37:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ print "Hello, World! A Change is about to begin... Stay tuned" . "\n";
 print "VT ROCKS!!!" . "\n";
 print "DO288 - Developer Track!". "\n";
 print "Everything is going to be alright!". "\n";
+print "Virtual Training";
 ?>
