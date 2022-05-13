1c9d55be3e5ac1e103a0bfc1f3a0f6fc1230dc96
sanjurajrh <84304865+sanjurajrh@users.noreply.github.com>
2022-05-13 07:32:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "Hello, World! php version is " . PHP_VERSION ."\n". "OpenShift ROCKS!!!";
-echo "Customers " ."\r\n"."Loves OpenShift";
+echo  nl2br("Hello, World! php version is " . PHP_VERSION ."\n". "OpenShift ROCKS!!!");
+echo nl2br("Customers " ."\r\n"."Loves OpenShift");
 ?>
