20b00ea9d04a2e9a8dc61c8250229ab211306768
sanjurajrh <84304865+sanjurajrh@users.noreply.github.com>
2022-07-07 13:02:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo  nl2br("Hello, World! php version is " . PHP_VERSION ."\n". "OpenShift ROCKS!!!");
+echo  nl2br("Hello, World! php version is " . PHP_VERSION ."\n". "OpenShift ROCKS!!!"."\n". "RHLSP Virtual Training!!!");
 ?>
